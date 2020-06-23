e3ee22979e3c57d0eab69073e6f07dcbb679b190
Shahan Neda <shahan.neda@gmail.com>
2020-06-23 13:35:13 -0700

    added ballon destoryer

---
+++ Assets/Scripts/BallloonDestroyer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallloonDestroyer : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+        
+    }
+}
