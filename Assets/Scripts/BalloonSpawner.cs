203b4561639306d28038a1a5726d3246537ca03f
Shahan Neda <shahan.neda@gmail.com>
2020-06-23 13:15:01 -0700

    added balloon spawning

---
+++ Assets/Scripts/BalloonSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonSpawner : MonoBehaviour
+{
+    private float lastSpawnTime = 0;
+    public float spawnInterval = 2f;
+
+    public GameObject balloonPrefab;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Time.time - lastSpawnTime > spawnInterval) {
+            SpawnNewBalloon();
+            lastSpawnTime = Time.time;
+        }
+
+    }
+
+
+    private void SpawnNewBalloon() {
+        Vector2 position = new Vector2(Random.RandomRange(-7, 7), transform.position.y);
+        GameObject balloon = Instantiate(balloonPrefab, position, Quaternion.identity, transform);
+        balloon.GetComponent<SpriteRenderer>().color = new Color(
+            Random.Range(0, 1.0f), 
+            Random.Range(0, 1.0f), 
+            Random.Range(0, 1.0f));
+
+
+    
+    }
+
+}
