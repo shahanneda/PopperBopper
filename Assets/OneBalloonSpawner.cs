035754b31081ee9888f271b533056bec3ad8f726
Shahan Neda <shahan.neda@gmail.com>
2020-07-25 22:03:25 -0700

    added spawing pad sprites

--- Assets/OneBalloonSpawner.cs
+++ Assets/OneBalloonSpawner.cs
@@ -8,34 +8,41 @@ public class OneBalloonSpawner : MonoBehaviour
 
     private GameObject balloon = null;
     private bool triggeredSpawn = false;
+
+    private Color padColor;
     // Start is called before the first frame update
     void Start()
     {
+        padColor = new Color(
+              Random.Range(0, 1.0f),
+              Random.Range(0, 1.0f),
+              Random.Range(0, 1.0f));
+
+        transform.GetChild(0).GetComponent<SpriteRenderer>().color = padColor;
         SpawnBalloon();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(balloon == null && !triggeredSpawn) { // triggeredSpawn check since sometimes in the period waiting for the couroutine it would start it 100 times
+
+        if (balloon == null && !triggeredSpawn)
+        { // triggeredSpawn check since sometimes in the period waiting for the couroutine it would start it 100 times
             triggeredSpawn = true;
-            StartCoroutine (balloonSpawnCoroutine()) ;
+            StartCoroutine(balloonSpawnCoroutine());
         }
 
     }
 
-    IEnumerator balloonSpawnCoroutine() { 
+    IEnumerator balloonSpawnCoroutine()
+    {
         yield return new WaitForSeconds(1);
         SpawnBalloon();
     }
-    void SpawnBalloon() { 
-          balloon = Instantiate(balloonPrefab, transform.position, Quaternion.identity, transform);
-          balloon.GetComponent<SpriteRenderer>().color = new Color(
-              Random.Range(0, 1.0f), 
-              Random.Range(0, 1.0f), 
-              Random.Range(0, 1.0f));
-
+    void SpawnBalloon()
+    {
+        balloon = Instantiate(balloonPrefab, transform.position, Quaternion.identity, transform);
+        balloon.GetComponent<SpriteRenderer>().color = padColor;
         triggeredSpawn = false; // since corutine is done we can reset it now;
     }
 }
