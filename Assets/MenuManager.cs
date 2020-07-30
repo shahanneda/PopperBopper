e36b996d5f6ba5c645a3a5ff01990e1b30efa637
Shahan Neda <shahan.neda@gmail.com>
2020-07-30 12:59:22 -0700

    made main menu and levels menu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public RectTransform levelsPanel;
+    public RectTransform playButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void PlayButtonClicked() {
+        levelsPanel.gameObject.SetActive(true);
+        playButton.gameObject.SetActive(false); 
+    }
+
+    public void BackButtonClicked() { 
+        levelsPanel.gameObject.SetActive(false);
+        playButton.gameObject.SetActive(true); 
+    }
+}
