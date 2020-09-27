3155150ec0b6c877643baf5ef5523a297583f1c7
Shahan Neda <shahan.neda@gmail.com>
2020-09-26 22:26:20 -0700

    tired to fix the menu

---
+++ Assets/MenuButtonContainer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButtonContainer : MonoBehaviour
+{
+    private RectTransform rect;
+    public RectTransform bottomOfScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        print(" anc " + rect.anchoredPosition);
+        print(" size " + rect.sizeDelta);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(" anc " + rect.anchoredPosition);
+        print(" size " + rect.sizeDelta);
+
+        print(" ancb " + bottomOfScreen.anchoredPosition);
+        print(" sizeb " + bottomOfScreen.sizeDelta);
+        
+    }
+}
